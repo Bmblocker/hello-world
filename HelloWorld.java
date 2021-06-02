5ad594683e8ed6fe93d6ebc49385a8c9a3d37ce4
Brandon Blocker <bblocke1@email.cpcc.edu>
2021-06-02 12:56:06 -0400

    Revert "Update HelloWorld.java"
    This reverts commit 173559ba1882d1622183e6fc04ac2b5acdcce062.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,7 @@ public class HelloWorld
    {
    
       
-      System.out.println("Hello world!");
-      System.out.println("\n\tProgram modified, but able to revert to original version using GitHub Desktop.");
+      System.out.print("Hello world!");
    
    }
 }
